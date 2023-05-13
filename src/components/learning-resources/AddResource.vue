d9e3cb45c3164978be26b0be8dbb0dbe07305e77
ossema90 <d.ossema.daoud@gmail.com>
2023-05-13 02:24:01 +0100

    submitting the application
    everything is done well here

---
+++ src/components/learning-resources/AddResource.vue
@@ -0,0 +1,85 @@
+<template>
+    <base-dialog v-if="inputIsInvalid" title="Invalid Input">
+        <template #default>
+            <p>At least input a value</p>
+        </template>
+        <template #actions>
+            <base-button @click="confirmError"> Close</base-button>
+        </template>
+    </base-dialog>
+    <base-card>
+        <form @submit.prevent="submitData">
+            <div class="form-control">
+                <label for="title">Title</label>
+                <input id="title" name="title" type="text" ref="titleInput">
+            </div>
+            <div class="form-control">
+                <label for="description">Description</label>
+                <textarea name="description" id="description" cols="30" rows="3" ref="descInput"></textarea>
+            </div>
+            <div class="form-control">
+                <label for="link">Link</label>
+                <input id="link" name="link" type="url" ref="linkInput">
+            </div>
+            <div>
+                <base-button type="submit">Add Resource</base-button>
+            </div>
+
+        </form>
+    </base-card>
+</template>
+
+<script>
+export default {
+    inject: ['addResource'],
+    data() {
+        return {
+            inputIsInvalid: false
+        }
+    },
+    methods: {
+        submitData() {
+            const entredTitle = this.$refs.titleInput.value;
+            const entredDescription = this.$refs.descInput.value;
+            const enteredUrl = this.$refs.linkInput.value;
+            if (entredTitle.trim() === '' || entredDescription.trim() === '' || enteredUrl.trim() === '') {
+                this.inputIsInvalid = true;
+                return;
+            }
+            this.addResource(entredTitle, entredDescription, enteredUrl);
+        },
+        confirmError() {
+            this.inputIsInvalid = false;
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+}
+
+input,
+textarea {
+    display: block;
+    width: 100%;
+    font: inherit;
+    padding: 0.15rem;
+    border: 1px solid #ccc;
+}
+
+input:focus,
+textarea:focus {
+    outline: none;
+    border-color: #3a0061;
+    background-color: #f7ebff;
+}
+
+.form-control {
+    margin: 1rem 0;
+}
+</style>
